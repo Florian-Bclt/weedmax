bc42e27b411944d8fe5b928258686612f9e82261
Florian Bouclet <bouclet.florian@hotmail.com>
2025-02-27 00:08:51 +0100

    init

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode}) {
       <Navbar />
       <CookieBanner />
       {typeof window !== "undefined" && localStorage.getItem("cookieConsent") === "true" && <GoogleTagManager />}
-      <main className="bg-gray-50 text-gray-800 flex-grow">{children}</main>
+      <main className="bg-gray-100 text-gray-800 flex-grow">{children}</main>
       <Footer />
     </div>
   )
