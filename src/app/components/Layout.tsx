b7c1e6f8f84603dfddfb3172a024fc2997a7aa36
Florian-Bclt <bouclet.florian@hotmail.com>
2025-04-14 09:30:19 +0200

    edit cookie

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,14 +1,12 @@
 import Navbar from "./Navbar"
 import CookieBanner from "./CookieBanner"
 import Footer from "./Footer"
-import GoogleTagManager from "./GoogleTagManager"
 
 export default function Layout({ children }: { children: React.ReactNode}) {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
       <Navbar />
       <CookieBanner />
-      {typeof window !== "undefined" && localStorage.getItem("cookieConsent") === "true" && <GoogleTagManager />}
       <main className="bg-gray-100 text-gray-800 flex-grow">{children}</main>
       <Footer />
     </div>
