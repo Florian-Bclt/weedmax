f0d52038adc2c4b16620afd5d33550857068477a
Florian-Bclt <bouclet.florian@hotmail.com>
2025-04-17 00:27:45 +0200

    fix to deploy

--- src/app/api/options/[id]/route.ts
+++ src/app/api/options/[id]/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { updateOption, deleteOption } from '@/lib/options/optionService';
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: any) {
   try {
     const { name } = await req.json();
-    const opt = await updateOption(params.id, name);
+    const opt = await updateOption(context.params.id, name);
     return NextResponse.json(opt);
   } catch (e: any) {
     return NextResponse.json({ error: e.message || 'Erreur' }, { status: 400 });
   }
 }
 
-export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(_: NextRequest, context: any) {
   try {
-    await deleteOption(params.id);
+    await deleteOption(context.params.id);
     return NextResponse.json({ message: 'Option supprimée' });
   } catch (e: any) {
     return NextResponse.json({ error: e.message || 'Erreur' }, { status: 400 });
