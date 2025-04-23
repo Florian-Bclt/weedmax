c0ac7d879eeb890be26084c546fd9de3b6f23ca3
Florian-Bclt <bouclet.florian@hotmail.com>
2025-04-23 09:47:03 +0200

    edit home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 "use client"
-import Image from "next/image";
 import Link from "next/link";
 import { motion } from 'framer-motion'
 import ButtonNeon from "./components/ButtonNeon";
 
 export default function Home() {
   return (
-    <main className="bg-black text-white relative flex items-center justify-center h-screen">
+    <main className="bg-black text-white relative flex flex-col items-center justify-center bg-[url('/images/test-bg.jpg')] bg-cover bg-center bg-no-repeat w-screen">
       <motion.div
         initial={{ opacity: 0, scale: 1.1 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1 }}
-        className="absolute top-2 transform flex flex-row justify-center items-center gap-4 md:gap-10 z-10 px-4 w-full"
+        className="absolute top-2 transform flex flex-row justify-center items-center mt-4 gap-4 md:gap-10 z-10 px-4 w-full"
       >
         <ButtonNeon 
           href='/products/nouveautes'
@@ -38,7 +37,7 @@ export default function Home() {
         </ButtonNeon>
       </motion.div>
 
-      {/* Image de fond animée */}
+      {/* Image de fond animée
       <motion.div
         initial={{ opacity: 0, scale: 1.1 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -46,12 +45,12 @@ export default function Home() {
         className="absolute inset-0 z-0"
       >
         <Image
-          src="/images/logo_weedmax.jpg"
+          src="/images/test-bg.jpg"
           alt="Hero Background"
           fill
           className="object-contain w-full h-full brightness-60 pt-14"
         />
-      </motion.div>
+      </motion.div> */}
 
       {/* Contenu textuel animé */}
       <motion.div
@@ -94,51 +93,56 @@ export default function Home() {
           </Link>
         </motion.div>
 
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 1.1 }}
-          className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-6 z-10 px-6"
-        >
-          {[
-            {
-              title: "CBD",
-              effet: 1,
-              description: "Détente douce, sans effet psychoactif",
-              color: "from-green-400 to-green-600",
-            },
-            {
-              title: "MCP-N",
-              effet: 2,
-              description: "Relaxation intense, idéal en fin de journée",
-              color: "from-purple-400 to-purple-600",
-            },
-            {
-              title: "Delta-BZ10",
-              effet: 3,
-              description: "Effet fort et rapide, pour utilisateurs avertis",
-              color: "from-pink-400 to-pink-600",
-            },
-          ].map((declinaison, idx) => (
-            <div
-              key={idx}
-              className={`flex flex-col items-center text-center bg-gradient-to-br ${declinaison.color} rounded-xl p-4 shadow-lg bg-opacity-30 border border-white/10 backdrop-blur transition-transform hover:scale-105 hover:brightness-110 duration-300`}
-            >
-              <h3 className="text-2xl font-bold mb-2">{declinaison.title}</h3>
-              <div className="flex justify-center items-center">
-                <p className="text-md uppercase tracking-wider mb-1 text-white/80">Effet</p>
-                <div className="flex justify-center mb-2">
-                  {Array.from({ length: declinaison.effet }).map((_, i) => (
-                    <span key={i} className="text-yellow-400 text-lg ml-2">★</span>
-                  ))}
-                </div>
+
+      </motion.div>
+
+      {/* Cards section */}
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 1.1 }}
+        className="my-10 grid grid-cols-1 md:grid-cols-3 gap-6 z-10 px-6"
+      >
+        {[
+          {
+            title: "CBD",
+            effet: 1,
+            description: "Détente douce, sans effet psychoactif",
+            textColor: "text-green-300",
+            neonColor: "shadow-[0_0_0_1px_#fff,inset_0_0_0_1px_#fff,0_0_3px_#34db34,0_0_5px_#34db34,0_0_10px_#34db34,0_0_15px_#34db34]",
+          },
+          {
+            title: "MCP-N",
+            effet: 2,
+            description: "Relaxation intense, idéal en fin de journée",
+            textColor: "text-green-300",
+            neonColor: "shadow-[0_0_0_1px_#fff,inset_0_0_0_1px_#fff,0_0_3px_#34db34,0_0_5px_#34db34,0_0_10px_#34db34,0_0_15px_#34db34]",
+          },
+          {
+            title: "Delta-BZ10",
+            effet: 3,
+            description: "Effet fort et rapide, pour utilisateurs avertis",
+            textColor: "text-green-300",
+            neonColor: "shadow-[0_0_0_1px_#fff,inset_0_0_0_1px_#fff,0_0_3px_#34db34,0_0_5px_#34db34,0_0_10px_#34db34,0_0_15px_#34db34]",
+          },
+        ].map((declinaison, idx) => (
+          <div
+            key={idx}
+            className={`flex flex-col items-center text-center bg-gradient-to-br ${declinaison.textColor} ${declinaison.neonColor} rounded-xl p-4 bg-opacity-30 border border-current backdrop-blur transition-transform hover:scale-105 hover:brightness-110 duration-300`}
+          >
+            <h3 className="text-2xl font-bold mb-2">{declinaison.title}</h3>
+            <div className="flex justify-center items-center">
+              <p className="text-md uppercase tracking-wider mb-1 text-white/80">Effet</p>
+              <div className="flex justify-center mb-2">
+                {Array.from({ length: declinaison.effet }).map((_, i) => (
+                  <span key={i} className="text-yellow-400 text-lg ml-2">★</span>
+                ))}
               </div>
-
-              <p className="text-lg text-white/90">{declinaison.description}</p>
             </div>
-          ))}
-        </motion.div>
 
+            <p className="text-lg text-white/90">{declinaison.description}</p>
+          </div>
+        ))}
       </motion.div>
     </main>
   );
